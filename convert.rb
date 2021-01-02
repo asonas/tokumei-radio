1f06640f93e4771a48ad70091235cbcac2b043ae
Yuya Fujiwara <asonas@cookpad.com>
2021-01-02 23:02:47 +0900

    Initial commit

---
+++ convert.rb
@@ -0,0 +1,22 @@
+require 'json'
+require 'pathname'
+require 'fileutils'
+
+Dir.glob("original/*.info.json").each do |file|
+  json = JSON.parse(File.read(file))
+  original_filename = Pathname.new json['_filename']
+  filename = if File.exists? original_filename
+    original_filename
+  else
+    if File.exists? original_filename.to_s.gsub(original_filename.extname, '.mkv')
+      original_filename.sub(original_filename.extname, '.mkv')
+    else
+      raise 'nai'
+    end
+  end
+
+  output_filename = filename.sub(filename.extname, '.mp3')
+  unless File.exists? output_filename
+    system "ffmpeg -i #{filename.to_s} -vn -ac 2 -ar 44100 -ab 192k -acodec libmp3lame -f mp3 #{output_filename}"
+  end
+end
